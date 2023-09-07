4e497e6a6a47766c3b66af606ca7c4d8faf9edd0
hyeri-woo <woohyeri0525@gmail.com>
2023-09-07 14:39:36 +0900

    Feat: 검색 화면 구현

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import { FaSearch } from 'react-icons/fa';
+import { useSearch } from '../context/SearchContext';
 
 export default function SearchForm() {
-  return <div>SearchForm</div>;
+  const { searchText, inputChange } = useSearch();
+
+  const searchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <StyledForm onSubmit={searchSubmit}>
+      <label>
+        <input type='text' name='sick' value={searchText} onChange={inputChange} autoFocus />
+        <FaSearch size='24' color='white' />
+      </label>
+      <button type='submit'>검색</button>
+    </StyledForm>
+  );
 }
+
+const StyledForm = styled.form``;
