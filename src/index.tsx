2a88b971350fb9a180968ffa370cc293f38a014f
hyeri-woo <woohyeri0525@gmail.com>
2023-09-07 11:32:41 +0900

    Initial: 프로젝트 초기 설정

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/Router';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<RouterProvider router={router} />);
