bbcfd7f95aca98885c35a12eb655a17d7b48e8cf
hyeri-woo <woohyeri0525@gmail.com>
2023-09-08 20:01:46 +0900

    Rename: cacheStorage 이동
    Co-authored-by: devshinn <shin.github@gmail.com>
    Co-authored-by: dmsgkr02 <dmsgkr02@naver.com>
    Co-authored-by: llbllhllk <byeonghyeon.dev@gmail.com>
    Co-authored-by: mrsimplelife <mrsimplelife@naver.com>
    Co-authored-by: nuri1042 <nuri100993@gmail.com>

--- src/storage/cacheStorage.ts
+++ src/storage/cacheStorage.ts
@@ -1,13 +1,14 @@
+import { Cache } from '../api/http';
+
 const EXPIRE_TIME = 24 * 60 * 60 * 1000; // 24시간
 
-class CacheApiServer {
+class CacheApiServer implements Cache {
   cacheName: string;
 
   constructor(cacheName: string) {
     this.cacheName = cacheName;
   }
-
-  async get(key: string) {
+  async get<T>(key: string): Promise<T | undefined> {
     const cache = await caches.open(this.cacheName);
     const response = await cache.match(key);
 
@@ -15,22 +16,20 @@ class CacheApiServer {
       if (this.isExpired(response)) {
         const request = new Request(key);
         await cache.delete(request);
-        return null;
+        return;
       } else {
-        return response;
+        return await response.json();
       }
     }
   }
 
-  async set(key: string, value: string) {
+  async set<T>(key: string, value: T): Promise<void> {
     const cache = await caches.open(this.cacheName);
     const header = new Headers();
     header.append('FETCH_DATE', new Date().toISOString());
 
     const response = new Response(JSON.stringify(value), { headers: header });
     cache.put(key, response);
-
-    console.info('calling api');
   }
 
   private isExpired(response: Response) {
@@ -42,3 +41,15 @@ class CacheApiServer {
 }
 
 export default CacheApiServer;
+
+export class MemoryCache implements Cache {
+  private storage: Map<string, any> = new Map();
+
+  async get<T>(key: string): Promise<T | undefined> {
+    return this.storage.get(key);
+  }
+
+  set<T>(key: string, value: T): void {
+    this.storage.set(key, value);
+  }
+}
